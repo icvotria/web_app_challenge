682c61b92f62dfb684822f369bc0a8f937238f36
icvotria <icvotria@gmail.com>
2022-01-04 22:56:54 +0000

    step 14

--- app.rb
+++ app.rb
@@ -8,3 +8,17 @@ end
 get '/secret' do
   'You know the thing about a shark? He\'s got lifeless eyes, black eyes, like a doll\'s eyes. When he comes at ya, doesn\'t seem to be livin\'... until he bites ya. And those black eyes roll over white, and then, oh, then you hear that terrible high-pitch screamin\', the ocean turns red, and spite of all the poundin\' and the hollerin\', they all come in and they rip you to pieces.'
 end
+
+get '/random_cat' do
+  @name = ['Rambo', 'Jones', 'Kirby'].sample
+  erb(:index)
+end
+
+get '/cat_form' do
+  erb(:form)
+end
+
+post '/named_cat' do
+  @name = params[:name]
+  erb(:index)
+end
